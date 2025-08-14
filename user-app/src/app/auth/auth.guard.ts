eafd0f4e8fa934db9f36e0e0329bce4e2fd616de
ban <banmejia@gmail.com>
2025-08-14 11:36:56 -0500

    fix auth guard

--- user-app/src/app/auth/auth.guard.ts
+++ user-app/src/app/auth/auth.guard.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
 
@@ -9,18 +8,19 @@ import { AuthService } from './auth.service';
  
 
 export class AuthGuard implements CanActivate{
+
   constructor(
     private auth: AuthService,
     private router: Router
     
   ){}
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    throw new Error('Method not implemented.');
-  }
   
-  canActive(): boolean {
-    if(this.auth.isAuthenticated()) return true;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.auth.isAuthenticated()){
+      return true;
+    } 
     this.router.navigate(['/login']);
     return false;
   }
+  
 }
